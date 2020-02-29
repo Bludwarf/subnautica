fa093626065fdeb7ac9c7ac84c0bad906329b56e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-29 13:28:33 +0000

    Deploy Bludwarf/subnautica to github.com/Bludwarf/subnautica.git:gh-pages

--- src/app/arrondi.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ArrondiPipe } from './arrondi.pipe';
-
-describe('ArrondiPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ArrondiPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
